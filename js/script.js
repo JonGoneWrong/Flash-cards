cc30d81c6c95c6ac78b83d6dc0bfd651271ed989
JonGoneWrong <jongonewrong@gmail.com>
2022-09-08 12:24:50 -0500

    Start building js

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/*----- constants -----*/
+/*
+currentCard
+addButton
+editButton
+deleteButton
+
+*/
+/*----- app's state (variables) -----*/
+/*
+incorrect cards
+3 most recent cards
+max streak count
+For stretch goals: decks the user has compiled.
+
+*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+/* 
+onclick for the 3 buttons, to add/edit/delete cards, 
+onclick to select a card,
+onhover to highlight a card? 
+something to flip a card?
+*/
+/*----- functions -----*/
+/*
+
+
+func streakCurrent {
+    count the number of 'correct' answers user has gotten consecutively 
+    grab the current-streak btn and set it to this number.
+}
+func streakMax {
+    if streakCurrent > 
+}
+*/
