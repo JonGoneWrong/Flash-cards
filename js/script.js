6842dfcca7436da7879ba099120f82406d59aada
JonGoneWrong <jongonewrong@gmail.com>
2022-09-12 17:40:10 -0500

    Build answer box and choices

--- js/script.js
+++ js/script.js
@@ -17,11 +17,11 @@ let currentCard = {
 	answer: null,
 };
 currentDeck = [
-	{ question: '2 x 3', answer: '6' },
-	{ question: '3 x 5', answer: '15' },
-	{ question: '5 x 8', answer: '40' },
-	{ question: '8 x 13', answer: '104' },
-	{ question: '13 x 21', answer: '273' },
+	{ question: '2 x 3', answer: 6, answerPool: [4, 5, 6, 9] },
+	{ question: '3 x 5', answer: 15, answerPool: [8, 12, 15, 20] },
+	{ question: '5 x 8', answer: 40, answerPool: [35, 40, 11, 12] },
+	{ question: '8 x 13', answer: 104, answerPool: [91, 96, 104, 112] },
+	{ question: '13 x 21', answer: 273, answerPool: [273, 291, 260, 283] },
 ];
 
 /*----- cached element references -----*/
@@ -30,23 +30,19 @@ const currentQuestion = document.querySelector('.current-question');
 
 const currentAnswer = document.querySelector('.current-answer');
 
+const answerBox = document.querySelector('answer-box');
+const answerChoices = document.querySelectorAll('.answer-choice');
+const firstAnswerChoice = document.querySelector('.a');
+const secondAnswerChoice = document.querySelector('.b');
+const thirdAnswerChoice = document.querySelector('.c');
+const fourthAnswerChoice = document.querySelector('.d');
+
 const addButton = document.querySelector('.add');
 const form = document.querySelector('form');
 const cancelButton = document.querySelector('.cancel');
 
 // const editButton = document.querySelector('.edit');
-
-const deleteButton = document.querySelector('.delete');
-
-const cardZero = document.querySelector('.zero');
-
-const cardOne = document.querySelector('.one');
-
-const cardTwo = document.querySelector('.two');
-
-const cardThree = document.querySelector('.three');
-
-const cardFour = document.querySelector('.four');
+// const deleteButton = document.querySelector('.delete');
 
 const deckScroll = document.querySelector('.deck-scroll');
 
@@ -58,10 +54,10 @@ function render() {
 		deckScroll.appendChild(card);
 		let cardQuestion = document.createElement('div');
 		let cardAnswer = document.createElement('div');
-		card.classList.add('card', 'zero');
+		card.classList.add('card');
 		card.setAttribute('id', `${i}`);
-		cardQuestion.classList.add('card-zero', 'question');
-		cardAnswer.classList.add('card-zero', 'answer');
+		cardQuestion.classList.add('question');
+		cardAnswer.classList.add('answer');
 		card.appendChild(cardQuestion);
 		card.appendChild(cardAnswer);
 		cardQuestion.innerHTML = currentDeck[i].question;
@@ -71,34 +67,38 @@ function render() {
 
 render();
 
-const hideQuestionsButton = document.querySelector('.hide-questions');
-const allQuestions = document.querySelectorAll('.question');
+// const hideQuestionsButton = document.querySelector('.hide-questions');
+// const allQuestions = document.querySelectorAll('.question');
 
+const showAnswersButton = document.querySelector('.show-answers');
 const hideAnswersButton = document.querySelector('.hide-answers');
-const allAnswers = document.querySelectorAll('.answer');
+// const allAnswers = document.querySelectorAll('.answer');
 
-const showBothButton = document.querySelector('.show-both');
+// const submitButton = document.querySelector('.submit');
 
 /*----- event listeners -----*/
+// answerBox.addEventListener('click', correctAnswer);
 
 form.addEventListener('submit', newCard);
 
 addButton.addEventListener('click', function () {
 	form.style.display = 'block';
+	cancelButton.style.display = 'block';
 	addButton.style.display = 'none';
-	deleteButton.style.display = 'none';
+	// deleteButton.style.display = 'none';
 });
 // editButton.addEventListener('click', editCardForm);
-deleteButton.addEventListener('click', deleteCurrentCard);
+// deleteButton.addEventListener('click', deleteCurrentCard);
 cancelButton.addEventListener('click', function () {
 	form.style.display = 'none';
+	cancelButton.style.display = 'none';
 	addButton.style.display = 'block';
-	deleteButton.style.display = 'block';
+	// deleteButton.style.display = 'block';
 });
 
-hideQuestionsButton.addEventListener('click', hideQuestions);
+// hideQuestionsButton.addEventListener('click', hideQuestions);
+showAnswersButton.addEventListener('click', showAnswers);
 hideAnswersButton.addEventListener('click', hideAnswers);
-showBothButton.addEventListener('click', showBoth);
 
 deckScroll.addEventListener('click', renderCard);
 
@@ -115,31 +115,34 @@ func streakMax {
     } 
 }
 */
-function newCard(e) {
-	e.preventDefault();
-	newCardQuestion = e.target.elements[0].value;
-	newCardAnswer = e.target.elements[1].value;
+function newCard(submit) {
+	submit.preventDefault();
+	newCardQuestion = submit.target.elements[0].value;
+	newCardAnswer = submit.target.elements[1].value;
 	let newCard = {
 		question: newCardQuestion,
 		answer: newCardAnswer,
 	};
 	currentDeck.push(newCard);
 	form.style.display = 'none';
+	cancelButton.style.display = 'none';
 	addButton.style.display = 'block';
-	deleteButton.style.display = 'block';
+	// deleteButton.style.display = 'block';
 
 	let card = document.createElement('div');
 	deckScroll.appendChild(card);
 	let cardQuestion = document.createElement('div');
 	let cardAnswer = document.createElement('div');
-	card.classList.add('card', 'zero');
+	card.classList.add('card');
 	card.setAttribute('id', `${currentDeck.length - 1}`);
-	cardQuestion.classList.add('card-zero', 'question');
-	cardAnswer.classList.add('card-zero', 'answer');
+	cardQuestion.classList.add('question');
+	cardAnswer.classList.add('answer');
 	card.appendChild(cardQuestion);
 	card.appendChild(cardAnswer);
 	cardQuestion.innerHTML = currentDeck[currentDeck.length - 1].question;
 	cardAnswer.innerHTML = currentDeck[currentDeck.length - 1].answer;
+	submit.target.elements[0].value = '';
+	submit.target.elements[1].value = '';
 }
 
 function renderCard(event) {
@@ -154,39 +157,61 @@ function renderCard(event) {
 	currentQuestion.innerText = currentCard.question;
 
 	currentAnswer.innerText = currentCard.answer;
+
+	let availableAnswers = [];
+	while (availableAnswers.length < 4) {
+		let newAnswer =
+			currentDeck[cardNumber].answerPool[Math.floor(Math.random() * 4)];
+		if (availableAnswers.includes(newAnswer)) {
+		} else {
+			availableAnswers.push(newAnswer);
+		}
+	}
+	console.log(availableAnswers);
+	//grab 4 buttons, assign each an innerText value pulled from currentDeck answerPool.
+	answerChoices.forEach(function (element, i) {
+		element.innerText = availableAnswers[i];
+	});
 }
 
 // function editCard() {
 // 	console.log('editing');
 // }
+// function deleteCurrentCard() {
+// 	console.log('deleting');
+// }
 
-function deleteCurrentCard() {
-	console.log('deleting');
-}
-
-function hideQuestions(e) {
-	// if (hideQuestionsButton.innerText == 'Hide Questions') {
-	// 	hideQuestionsButton.innerText = 'Show Questions';
-	// } else {
-	// 	hideQuestionsButton.innerText = 'Hide Questions';
-	// }
-	allQuestions.forEach(function (element) {
-		element.style.visibility = 'hidden';
+// function hideQuestions(e) {
+// if (hideQuestionsButton.innerText == 'Hide Questions') {
+// 	hideQuestionsButton.innerText = 'Show Questions';
+// } else {
+// 	hideQuestionsButton.innerText = 'Hide Questions';
+// }
+// const allQuestions = document.querySelectorAll('.question');
+// allQuestions.forEach(function (element) {
+// 	element.style.visibility = 'hidden';
+// });
+// }
+function showAnswers(e) {
+	// const allQuestions = document.querySelectorAll('.question');
+	// allQuestions.forEach(function (element) {
+	// 	element.style.visibility = 'visible';
+	// });
+	const allAnswers = document.querySelectorAll('.answer');
+	allAnswers.forEach(function (element) {
+		element.style.visibility = 'visible';
 	});
 }
 function hideAnswers(e) {
+	const allAnswers = document.querySelectorAll('.answer');
 	allAnswers.forEach(function (element) {
 		element.style.visibility = 'hidden';
 	});
 }
-function showBoth(e) {
-	allQuestions.forEach(function (element) {
-		element.style.visibility = 'visible';
-	});
-	allAnswers.forEach(function (element) {
-		element.style.visibility = 'visible';
-	});
-}
+
+// function evaluateAnswer {
+// 	if
+// }
 
 /*POSSIBLE SOLUTION TO CARD SELECTION USING ARROW KEYS?:
 https://stackoverflow.com/questions/5597060/detecting-arrow-key-presses-in-javascript
